2ac4659d0ba9d2ab232a369b138cf00a72aa1b6f
Lin Čadež <68005465+lin-cadez@users.noreply.github.com>
2022-11-18 20:15:55 +0100

    file

---
+++ reminder bot.py
@@ -0,0 +1,33 @@
+import discord
+import datetime
+import requests
+from lxml import html
+from discord.ext import commands,tasks
+intents = discord.Intents().all()
+
+client = commands.Bot(command_prefix='!',intents=intents)
+cajt=datetime.datetime.today().weekday()
+myid = '@everyone'
+print(cajt)
+
+
+@client.event
+async def on_ready():
+	channel = client.get_channel(1040656031807701062) #channel id here
+	print(channel)
+
+	if cajt==1 or cajt==0:	
+		embed = discord.Embed(
+	        title="Opomnik za KROKY ❤😍❤",
+	        description="Naroč si malco! Priporočam!!!")
+		embed.add_field(name="Urbi priporoča!", value="BUREK❤❤❤")
+		await channel.send(embed=embed)
+		await channel.send(myid)
+
+
+
+
+
+if __name__ == "__main__" :
+	client.run("MTA0MDY1Mjk2MTY1MjY5MDk4NQ.GHRYUk.Nzfvkx6C5AAlB6xygTsqhqCfkEqzfaUGqGcnL4")
+
